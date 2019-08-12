40087cae8c1a16f54a878aa53be8a3d0da3d90be
Kun Yang <kunya@microsoft.com>
2019-08-12 21:48:03 +0800

    Add test cases for current BFS for brick list

--- ImageConverter.py
+++ ImageConverter.py
@@ -86,26 +86,6 @@ class ImageConverter():
             csvReader = csv.reader(csvfile)
             for row in csvReader:
                 self.BrickSizes.append((int(row[0]), int(row[1])))
-    
-    def GetBricksFromBrickList(self, brickList, lastBrick):
-        brickDict = { lastBrick : 1 }
-        for brick in brickList:
-            if brick in brickDict:
-                brickDict[brick] += 1
-            else:
-                brickDict[brick] = 1
-        return brickDict
-    
-    def GetBrickListGivenState(self, currentState, stateHistory):
-        currentStep = currentState[0]
-        currentPointsSet = set(currentState[1:])
-        for state, brickList in stateHistory.items():
-            step = state[0]
-            if step == currentStep:
-                pointsSet = set(state[1:])
-                if pointsSet == currentPointsSet:
-                    return brickList
-        return []
 
     def GetLeftTopPoint(self, points):
         x = min([point[0] for point in points])
@@ -124,8 +104,8 @@ class ImageConverter():
         currentBrickList.append(brick)
         return currentPointSet, currentBrickList
 
-    def GetBrickListForConnectedComponent2(self, pointsInCurrentComponentSet):
-        stateHistory = [({}, [])]
+    def GetBrickListForConnectedComponent(self, pointsInCurrentComponentSet):
+        stateHistory = [(set([]), [])]
         startIndex = 0
         endIndex = 1
         while startIndex < endIndex:
@@ -140,71 +120,29 @@ class ImageConverter():
                 if (maxBrickSize >= brick[0] * brick[1]):
                     (resultState1, resultBrickList1) = self.CheckIfThisBrickIsGood(pointsInLastState, brickListOfLastState, startPoint, brick, brick[0], brick[1], availablePoints)
                     if resultState1 is not None:
+                        if len(resultState1) == len(pointsInCurrentComponentSet):
+                            return resultBrickList1
                         stateHistory.append((resultState1, resultBrickList1))
                     if brick[0] != brick[1]:
                         (resultState2, resultBrickList2) = self.CheckIfThisBrickIsGood(pointsInLastState, brickListOfLastState, startPoint, brick, brick[1], brick[0], availablePoints)
                         if resultState2 is not None:
+                            if len(resultState2) == len(pointsInCurrentComponentSet):
+                                return resultBrickList2
                             stateHistory.append((resultState2, resultBrickList2))
             endIndex = len(stateHistory)
             startIndex += 1
 
-    def GetBrickListForConnectedComponent(self, pointsInCurrentComponentSet):
-        # CPU usage is super high, needs improvement
-        stateHistory = { (0,) : [] }
-        totalSize = len(pointsInCurrentComponentSet)
-        for step in range(1, totalSize + 1):
-            # type: a list of sets
-            lastStepStates = [set(x[1:]) for x in stateHistory.keys() if x[0] == step -1]
-            for lastStepState in lastStepStates:
-                # type: a set of points
-                availableStartPoint = pointsInCurrentComponentSet - lastStepState
-                lastStepStateList = list(lastStepState)
-                lastStepStateList.insert(0, step - 1)
-                lastStepStateTuple = tuple(lastStepStateList)
-                for point in availableStartPoint:
-                    for brick in self.BrickSizes:
-                        if (len(availableStartPoint) < brick[0] * brick[1]):
-                            continue
-                        # type: a set of points
-                        currentState = deepcopy(lastStepState)
-                        for i in range(0, brick[0]):
-                            for j in range(0, brick[1]):
-                                currentState.add((point[0] + i, point[1]+j))
-                        if currentState == pointsInCurrentComponentSet:
-                            return self.GetBricksFromBrickList(self.GetBrickListGivenState(lastStepStateTuple, stateHistory), brick)
-                        if currentState < pointsInCurrentComponentSet:
-                            if currentState not in [set(x[1:]) for x in stateHistory.keys() if x[0] == step]:
-                                tempCurrentStateList = list(currentState)
-                                tempCurrentStateList.insert(0, step)
-                                lastStepStateBrickList = deepcopy(self.GetBrickListGivenState(lastStepStateTuple, stateHistory))
-                                lastStepStateBrickList.append(brick)
-                                # value is brick list
-                                stateHistory[tuple(tempCurrentStateList)] = lastStepStateBrickList
-        return None 
-
     def UpdateTotalBrickDict(self, color, pointsInCurrentComponentSet):
         try:
-            brickDictForCurrentConnectedComponent = self.GetBrickListForConnectedComponent(pointsInCurrentComponentSet)
-            if brickDictForCurrentConnectedComponent is not None:
+            brickList = self.GetBrickListForConnectedComponent(pointsInCurrentComponentSet)
+            if brickList is not None:
                 self.lock.acquire()
-                for brick in brickDictForCurrentConnectedComponent:
+                for brick in brickList:
                     if (brick, color) not in self.TotalBrickDict:
                         self.TotalBrickDict[(brick, color)] = 0
-                    self.TotalBrickDict[(brick, color)] += brickDictForCurrentConnectedComponent[brick]
+                    self.TotalBrickDict[(brick, color)] += 1
         finally:
             self.lock.release()
-
-    def GetBrickListForImage(self):
-        TotalBrickDict = {}
-        for startPoint in self.ConnectedComponents:
-            color = self.Matrix[startPoint[0]][startPoint[1]]
-            brickDictForCurrentConnectedComponent = self.GetBrickListForConnectedComponent(self.ConnectedComponents[startPoint])
-            if brickDictForCurrentConnectedComponent is not None:
-                for brick in brickDictForCurrentConnectedComponent:
-                    if (brick, color) not in TotalBrickDict:
-                        TotalBrickDict[(brick, color)] = 0
-                    TotalBrickDict[(brick, color)] += brickDictForCurrentConnectedComponent[brick]
-        return TotalBrickDict
 
     def GetBrickListForImageMultiThreads(self):
         threads = []
@@ -221,10 +159,14 @@ if __name__=='__main__':
     imageConverter.GetBrickColors()
     imageConverter.GetBrickSizes()
     #pointsInSet = set([(0,0), (0,1), (0,2), (1,1),(1,2),(1,3)])
+    #pointsInSet = set([(0,0), (1,0),(2,0),(1,1),(2,1),(3,1)])
     #imageConverter.GetBrickListForConnectedComponent(pointsInSet)
     imageConverter.ResizeImage((16, 16))
     imageConverter.ConvertImageToBricks()
     imageConverter.GetAllConnectedComponents()
     imageConverter.targetIm.show()
-    brickDict = imageConverter.GetBrickListForImageMultiThreads()
-    print(len(brickDict))
+    imageConverter.GetBrickListForImageMultiThreads()
+    for key in imageConverter.TotalBrickDict:
+        print(key, imageConverter.TotalBrickDict[key])
+
+
